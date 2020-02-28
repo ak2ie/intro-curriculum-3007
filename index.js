3a14f862c637b20c599f977e256381cf27a5e6b7
ak2ie <mail@ak2ie.net>
2020-02-29 02:10:22 +0900

    17で割り切れるか確認する処理を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-'use strict';
+"use strict";
 
-module.exports = {
+/**
+ * 整数が17で割りきれるかを返す（割り切れるならtrue）
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+  if (num % 17 === 0) {
+    return true;
+  }
+  return false;
 }
+module.exports = { isMultipleOfSeventeen };
